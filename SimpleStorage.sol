1c9501bc973dde30a0bc447554ee9ce64057aee5
jessig1 <jessig120@gmail.com>
2024-09-06 11:51:06 -0400

    simple test

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.6.0 <0.9.0;
 
 contract SimpleStorage {
@@ -9,5 +10,29 @@ contract SimpleStorage {
     // int256 favoriteInt = -5;
     // address favoriteAddress = 0x0c769EDE18bf11389A106f462095d33D1339d5d0;
     // bytes32 favoriteBytes = "dog"; 
+    uint256 public favoriteNumber;
+    bool favoriteBool;
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    //People public person = People({favoriteNumber: 2, name: "Jake"});
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
     
+    function retrieve() public view returns(uint256){
+        return favoriteNumber;
+    }
+
+    //storage = bytes stored "forever" memory = bytes only when executed
+    function addPerson(string memory _name, uint256 _favoriteNumber) public{
+        people.push(People(_favoriteNumber, _name));
+    }
 }
