7c064aa65f8a9c817df23748944f5f07e753921b
Jakob Essig <essigj88@outlook.com>
2024-09-05 21:55:18 -0400

    first commit

---
+++ SimpleStorage.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.6.0 <0.9.0;
+
+contract SimpleStorage {
+
+    // types
+   // uint256 favoriteNumber = 5;
+    // bool favoriteBool = true;
+    // string favoriteString = "a string";
+    // int256 favoriteInt = -5;
+    // address favoriteAddress = 0x0c769EDE18bf11389A106f462095d33D1339d5d0;
+    // bytes32 favoriteBytes = "dog"; 
+    
+}
